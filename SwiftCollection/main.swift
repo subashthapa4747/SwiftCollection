cbecd640640be00a23d176c901cb28f6f95f99a7
subash <thapa.subash345@gmail.com>
2020-02-24 02:48:49 -0800

    Initial Commit

---
+++ SwiftCollection/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftCollection
+//
+//  Created by subash on 2/24/20.
+//  Copyright © 2020 symbolicTrace. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
